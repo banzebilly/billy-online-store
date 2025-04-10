0f90cf0c3b9922e77c873f4a17568322876ddb77
banzebilly <bbanze49gmail.com>
2025-04-10 10:39:21 +0200

    deploying the app int s3 bucket

--- billy_online_store/settings.py
+++ billy_online_store/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['django-env.eba-cqn8igrm.af-south-1.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['django-env.eba-cqn8igrm.af-south-1.elasticbeanstalk.com', '*']
 
 
 
@@ -49,8 +49,7 @@ INSTALLED_APPS = [
     'store',
     'orders',
       # other apps...
-    # 'admin_honeypot',
-
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -116,6 +115,9 @@ else:
 
 
 
+
+
+
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
@@ -150,18 +152,39 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+AWS_S3_REGION_NAME = "af-south-1"
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
+# STATIC_ROOT = BASE_DIR / 'static'
+
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'billy_online_store/static',  # Add this if you have additional static files here
+#
+
+# AWS S3 Static Files Configuration
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = 'public-read'
+AWS_LOCATION = 'static'
 STATIC_ROOT = BASE_DIR / 'static'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'billy_online_store/static',  # Add this if you have additional static files here
+    'billy_online_store/static',
 ]
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
+DEFAULT_FILE_STORAGE = 'billy_online_store.media_backends.MediaStorage'
 
+# media files configuration
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
- 
+MEDIA_ROOT = BASE_DIR /'media'
 # Django messages configuration
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
