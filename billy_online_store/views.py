686a03959de3510a3729aacc1b249b6341aadeeb
banzebilly <bbanze49gmail.com>
2025-04-11 19:04:13 +0200

    i added paginator to the home page

--- billy_online_store/views.py
+++ billy_online_store/views.py
@@ -11,15 +11,19 @@
 #     return render(request, 'home.html', context)
 from django.shortcuts import render
 from store.models import Product, ReviewRating
-
+from django.core.paginator import PageNotAnInteger,EmptyPage, Paginator
 def home(request):
     products = Product.objects.filter(is_available=True).order_by('created_date')
-
+    
     # Get reviews for all products
     reviews_dict = {product.id: ReviewRating.objects.filter(product_id=product.id, status=True) for product in products}
+    
+    paginator = Paginator(products, 6)
+    page = request.GET.get('page')
+    paged_products = paginator.get_page(page)
 
     context = {
-        'products': products,
+        'products': paged_products,
         'reviews': reviews_dict,
     }
     return render(request, 'home.html', context)
